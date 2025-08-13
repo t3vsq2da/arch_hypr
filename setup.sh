db32d2530706a129767e829fc3329efe1a8856f8
- <->
2025-08-13 21:09:33 +0500

    1

--- setup.sh
+++ setup.sh
@@ -1,1 +1,20 @@
-hi!
+#hypr
+sudo pacman --noconfirm -S hyprland wayland-protocols nvidia noto-fonts
+#git
+sudo pacman --noconfirm -S git
+#yay
+git clone https://aur.archlinux.org/yay.git ~/app_srcs/yay
+cd ~/yay
+makepkg si
+cd -
+#sound
+sudo pacman --noconfirm -S pulseaudio
+#macro
+sudo pacman -S --noconfirm ydotool xorg-xinput
+#js
+sudo pacman -S --noconfirm nodejs npm
+#apps
+sudo pacman -S --noconfitm kitty pavucontrol
+yay -S google-chrome
+
+
